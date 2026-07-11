75554f444f3d2dbe148e730f5dba776afe0b7d36
Tejas_ <tejasjalit@gmail.com>
2026-07-11 12:23:59 +0530

    env variable changes

--- backend/db.js
+++ backend/db.js
@@ -1,20 +1,21 @@
-require("dotenv").config();
 const mysql = require("mysql2");
+require("dotenv").config();
 
 const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    port: Number(process.env.MYSQLPORT),
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
 
 db.connect((err) => {
     if (err) {
-        console.error(err);
-    } else {
-        console.log("MySQL Connected");
+        console.log("MySQL connection failed:", err.message);
+        return;
     }
+
+    console.log("MySQL Connected");
 });
 
 module.exports = db;
